e1ecdc18f5f4a4fb75cba2a5524577e292d3ab4e
29td <nonhlanhlamndebele9@gmail.com>
2022-11-02 14:45:33 +0200

    processing data in models

--- app/models/comments.rb
+++ app/models/comments.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
-  
-    after_save :comment_counter
-  
-    def comment_counter
-      post.increment!(:comments_counter)
-    end
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :comment_counter
+
+  def comment_counter
+    post.increment!(:comments_counter)
   end
+end
--- app/models/posts.rb
+++ app/models/posts.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :likes
-    has_many :comments
-  
-    after_save :post_counter
-  
-    def recent_comments
-      Comment.limit(5).order(created_at: :desc)
-    end
-  
-    private
-  
-    def post_counter
-      user.increment!(:PostsCounter)
-    end
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :post_counter
+
+  def recent_comments
+    Comment.limit(5).order(created_at: :desc)
   end
+
+  private
+
+  def post_counter
+    user.increment!(:PostsCounter)
+  end
+end
