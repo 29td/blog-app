172644467d5b4d0dd6d2c7fc5119aed75a07b09b
29td <nonhlanhlamndebele9@gmail.com>
2022-11-09 08:28:12 +0200

    Added views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :post
 
   after_save :comment_counter
 
   def comment_counter
-    post.increment!(:comment_counter)
+    post.increment!(:comments_counter)
   end
 end
