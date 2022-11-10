3a7ff1f9d3df2c1d87fe72e9cf3207592d8f583e
29td <nonhlanhlamndebele9@gmail.com>
2022-11-10 12:21:02 +0200

    Added form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes
+  has_many :likes, foreign_key: 'post_id'
   has_many :comments
 
   after_save :post_counter
