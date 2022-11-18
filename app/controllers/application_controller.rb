fc0250fe6a1c18f821134f251822e2bd8713b390
29td <nonhlanhlamndebele9@gmail.com>
2022-11-18 17:10:44 +0200

    Added rest API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,32 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:name, :email, :password, :password_confirmation)
+  def json_response(json, status)
+    render json:, status:
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :surname, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+  # Catch all CanCan errors and alert the user of the exception
+  def after_sign_out_path_for(_resource_or_scope)
+    '/users/sign_in'
+  end
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 end
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,16 @@
+class AuthenticationController < ApplicationController
+    # skip_before_action :authenticate_request
+    # puts params[:email]
+    skip_before_action :verify_authenticity_token
+  
+    def authenticate
+      command = AuthenticateUser.call(params[:email], params[:password])
+  
+      if command.success?
+        render json: { auth_token: command.result }
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
+    end
+  end
+  
