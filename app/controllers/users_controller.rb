db10e85d801eabb56a0e4047cf564fddec7fc832
29td <nonhlanhlamndebele9@gmail.com>
2022-11-18 08:20:09 +0200

    Added devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  protect_from_forgery
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
   end
 end
