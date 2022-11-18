21775d4d1256010ebb54884f96648df53bdb25d0
29td <nonhlanhlamndebele9@gmail.com>
2022-11-18 17:15:45 +0200

    Added rest API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, sign_out_via: [:get, :post]
+  # get 'likes/create'
+  # get 'comments/create'
   resources :users do 
     resources :posts 
   end 
@@ -8,4 +10,14 @@ Rails.application.routes.draw do
      resources :likes 
   end
   root to: "users#index"
+  post 'authenticate', to: 'authentication#authenticate'
+  namespace :api do
+    resources :users do 
+      resources :posts 
+    end 
+    resources :posts do
+       resources :comments 
+       resources :likes 
+    end
+  end
 end
