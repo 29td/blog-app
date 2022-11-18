8e72756d2888318c5c8b2bcdd4675e7ba1ac8823
29td <nonhlanhlamndebele9@gmail.com>
2022-11-18 11:13:15 +0200

    Add authorization and design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:name, :email, :password, :password_confirmation)
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :surname, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+  def after_sign_out_path_for(_resource_or_scope)
+    '/users/sign_in'
   end
 end
